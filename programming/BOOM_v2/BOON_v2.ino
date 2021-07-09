53ca0090f6af73ee31c66d95f2fce2880fcf64cc
gogomaker <daseulgi100@gmail.com>
2021-07-09 22:16:50 +0900

    Make basic codes 기본적 코드 및 주석 정리 완료

--- programming/BOOM_v2/BOON_v2.ino
+++ programming/BOOM_v2/BOON_v2.ino
@@ -1,8 +1,17 @@
-//BOOM_v2.ino
-//이 프로그램은 v1의 개선판이다.
+/*
+    name   : BOOM_v2.ino
+    author : gogomaker
+    explanation-
+    이 프로그램은 v1의 개선판이다.
+    하드웨어적으로 바뀌는 부분이 있기에, 코드도 상당히 바뀔 것이다.
+*/
+
+#include "src/functions.h"
+
+//젼역변수 선언
 
 void setup() {
-
+    Serial.begin(115200);
 }
 
 void loop() {
---
+++ programming/BOOM_v2/src/functions.h
@@ -0,0 +1,12 @@
+#ifndef __FUNCTIONS_H__
+# define __FUNCTIONS_H__
+
+#include <Arduino.h>
+
+//상수선언
+
+//전역변수선언
+
+//함수선언
+
+#endif
